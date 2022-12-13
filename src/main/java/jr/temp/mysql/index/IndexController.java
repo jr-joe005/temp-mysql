e2c0ce99e6b1459b1434e56023e01da8acbf1dcb
jiangrui <jr_joe@foxmail.com>
2022-12-13 17:58:35 +0800

    20221213-03

--- src/main/java/jr/temp/mysql/index/IndexController.java
+++ src/main/java/jr/temp/mysql/index/IndexController.java
@@ -1,17 +1,33 @@
 package jr.temp.mysql.index;
 
+import jr.temp.mysql.cmn.entity.Person;
+import jr.temp.mysql.cmn.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 测试用画面Controller
  */
+@Slf4j
 @Controller
 public class IndexController {
+
+    /** 测试用表Repository */
+    @Autowired
+    PersonRepository personRepository;
 
     /** Index页 */
     @GetMapping("/")
     public String index(){
+
+        /* 获取DB数据 */
+        Person person = personRepository.getReferenceById(100002);
+        log.info("---- [IndexController.index] -- id     : {}", person.getId());
+        log.info("---- [IndexController.index] -- name   : {}", person.getName());
+        log.info("---- [IndexController.index] -- age    : {}", person.getAge());
+        log.info("---- [IndexController.index] -- address: {}", person.getAddress());
 
         return "index";
     }
