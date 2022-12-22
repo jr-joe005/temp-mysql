dc1a12935507fefe1c09c1f1bdb7485c092e6043
jr_joe005 <jr_joe@foxmail.com>
2022-12-22 13:40:54 +0800

    20221222-01

--- docker_config/hashicorp_vault/config/config_server.sh
+++ docker_config/hashicorp_vault/config/config_server.sh
@@ -10,26 +10,20 @@ vault status
 # 创建secret engine：database
 vault secrets enable database
 
-# 创建与Postgresql的连接
+# 创建与Mysql的连接
 vault write database/config/test_db \
       plugin_name="mysql-database-plugin" \
+      connection_url="{{username}}:{{password}}@tcp(192.168.2.3:3306)/test_db" \
       allowed_roles="dbuser" \
-      connection_url="{{username}}:{{password}}@tcp(192.168.3.137:3306)/test_db" \
       username="root" \
-      password="password" \
-      tls_certificate_key=@/vault/ssl_file/client-tls.pem \
-      tls_ca=@/vault/ssl_file/ca.pem
+      password="password"
 
-# 创建Vault凭证角色
-
+# 设置动态密钥策略
 vault write database/roles/dbuser \
     db_name="test_db" \
-    default_ttl="1h" \
-    max_ttl="24h" \
     creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT,INSERT,UPDATE ON *.* TO '{{name}}'@'%';" \
+    default_ttl="1h" \
+    max_ttl="24h"
 
-# 创建设置动态密钥策略
-#vault policy write my_policy /vault/config/policy.hcl
-
-# 使用创建的Postgresql连接和Vault凭证角色，创建并查看Postgresql登录账号和密码
+# 使用创建的Mysql连接和Vault凭证角色，创建并查看Mysql登录账号和密码
 vault read database/creds/dbuser
---
+++ docker_config/run_server.ps1
@@ -0,0 +1,16 @@
+# 停止/移除既存容器
+docker stop mysql-db-server
+docker stop vault-server
+docker rm mysql-db-server
+docker rm vault-server
+
+# 启动容器,并初期化Postgresql
+docker-compose up -d
+Start-Sleep -s 60
+docker exec -it mysql-db-server bash /etc/mysql/init/init_server.sh
+
+# 重新启动并进入容器，查看实时日志
+Start-Sleep -s 5
+docker restart mysql-db-server
+docker restart vault-server
+docker-compose logs -f
